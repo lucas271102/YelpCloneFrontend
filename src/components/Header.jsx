73259cf63dc6cd83889d7495116bd6c47fda3e33
lucas271102 <lucas001spe@gmail.com>
2023-10-03 15:41:36 -0300

    components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-          <h1 className='text-3xl font-light'>Restaurant Finder</h1>
+          <h1 className='text-black text-5xl font-light '>Restaurant Finder</h1>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const {nextui} = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",],
+  content: [  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}","./src/**/*.{html,jsx}",],
   theme: {
     extend: {},
   },
